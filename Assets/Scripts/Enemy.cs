05317b0ad7d03f49bc1155d56bc8a7aeec719be5
NoisyBass <laura.dcs4@gmail.com>
2017-08-29 21:00:36 +0200

    Basic enemiy spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private float _enemySpeed = 2.0f;
+
+    private Transform _target;
+    public Transform Target
+    {
+        set { _target = value; }
+    }
+	
+	void Update () {
+        Vector2 direction = (_target.position - transform.position).normalized;
+        Vector3 movement = direction * _enemySpeed * Time.deltaTime;
+        transform.position += movement;
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Enemy _enemyPrefab;
+    [SerializeField]
+    private Transform _enemiesTarget;
+    [SerializeField]
+    private float _spawnSeconds = 2.0f;
+    [SerializeField]
+    private float _spawnDistance = 10.0f;
+    private Vector2 _center;
+
+    void Awake () {
+        _center = transform.position;
+	}
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            Vector3 spawnPos = Vector3.zero;
+            spawnPos.x = _spawnDistance * Random.Range(-1.0f, 1.0f) + _center.x;
+            spawnPos.y = _spawnDistance * Random.Range(-1.0f, 1.0f) + _center.y;
+            Enemy enemy = Instantiate(_enemyPrefab, spawnPos, Quaternion.identity);
+            enemy.Target = _enemiesTarget;
+            yield return new WaitForSeconds(_spawnSeconds);
+        }
+    }
+}
