1ccab48884b76de07d47123ad5bc953c9f2d5e41
NoisyBass <laura.dcs4@gmail.com>
2017-08-30 20:05:29 +0200

    GameManager created

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+    [System.Serializable]
+    public struct SettingsData
+    {
+        public bool controller;
+    }
+
+    [SerializeField]
+    private SettingsData _settings;
+    public SettingsData Settings
+    {
+        get { return _settings; }
+    }
+
+    //protected override void Awake()
+    //{
+    //    base.Awake();
+    //}
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
