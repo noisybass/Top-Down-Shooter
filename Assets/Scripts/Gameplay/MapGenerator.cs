28b212136f427a0d20339846c716d8b78350dcde
NoisyBass <laura.dcs4@gmail.com>
2018-01-10 21:11:52 +0000

    Creating map generator

---
+++ Assets/Scripts/Gameplay/MapGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MapGenerator {
+
+    struct Cell
+    {
+        int x, y;
+    }
+
+    protected int[,] _map;
+    protected int _width;
+    protected int _height;
+    protected int _fillPercentage;
+    protected bool _autoSmoothing;
+    protected int _smoothing;
+
+    public MapGenerator()
+    {
+        _map = new int[10, 10];
+        _width = 10;
+        _height = 10;
+        _fillPercentage = 50;
+        _autoSmoothing = false;
+        _smoothing = 5;
+    }
+
+    public MapGenerator(int width, int height, int fillPercentage, bool autoSmoothing, int smoothing)
+    {
+        _map = new int[width, height];
+        _width = width;
+        _height = height;
+        _fillPercentage = fillPercentage;
+        _autoSmoothing = autoSmoothing;
+        _smoothing = smoothing;
+    }
+
+    public abstract int[,] GenerateMap();
+    public abstract int[,] SmoothMap();
+    public abstract int[,] CleanMapWalls(int wallThresholdSize);
+}
---
+++ Assets/Scripts/Gameplay/RandomWalkGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalkGenerator : MapGenerator
+{
+    public RandomWalkGenerator()
+        : base()
+    {
+    }
+
+    public RandomWalkGenerator(int width, int height, int fillPercentage, bool autoSmoothing, int smoothing)
+        : base(width, height, fillPercentage, autoSmoothing, smoothing)
+    {
+    }
+
+    public override int[,] CleanMapWalls(int wallThresholdSize)
+    {
+        return _map;
+    }
+
+    public override int[,] GenerateMap()
+    {
+        return _map;
+    }
+
+    public override int[,] SmoothMap()
+    {
+        return _map;
+    }
+}
