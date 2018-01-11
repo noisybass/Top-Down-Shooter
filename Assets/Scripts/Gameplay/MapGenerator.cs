a40bcd9a8f3916ab268d8c64b0926a320453a6b8
NoisyBass <laura.dcs4@gmail.com>
2018-01-11 21:13:48 +0000

    Added random walk generator

--- Assets/Scripts/Gameplay/MapGenerator.cs
+++ Assets/Scripts/Gameplay/MapGenerator.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public abstract class MapGenerator {
 
-    struct Cell
+    public struct Cell
     {
-        int x, y;
+        public int x, y;
+
+        public Cell(int xx, int yy)
+        {
+            x = xx;
+            y = yy;
+        }
     }
 
     protected int[,] _map;
     protected int _width;
     protected int _height;
     protected int _fillPercentage;
-    protected bool _autoSmoothing;
     protected int _smoothing;
 
     public MapGenerator()
@@ -22,21 +27,50 @@ public abstract class MapGenerator {
         _width = 10;
         _height = 10;
         _fillPercentage = 50;
-        _autoSmoothing = false;
         _smoothing = 5;
     }
 
-    public MapGenerator(int width, int height, int fillPercentage, bool autoSmoothing, int smoothing)
+    public MapGenerator(int width, int height, int fillPercentage, int smoothing)
     {
         _map = new int[width, height];
         _width = width;
         _height = height;
         _fillPercentage = fillPercentage;
-        _autoSmoothing = autoSmoothing;
         _smoothing = smoothing;
     }
 
     public abstract int[,] GenerateMap();
     public abstract int[,] SmoothMap();
     public abstract int[,] CleanMapWalls(int wallThresholdSize);
+
+    protected int GetNeighborsNumber(int x, int y, int stepX, int stepY)
+    {
+        int neighbors = 0;
+
+        int minX = x - stepX;
+        int maxX = x + stepX;
+        int minY = y - stepY;
+        int maxY = y + stepX;
+
+        for (int i = minX; i <= maxX; i++)
+        {
+            for (int j = minY; j <= maxY; j++)
+            {
+                if (!IsOutOfBounds(i, j))
+                {
+                    if (i != x || j != y)
+                        neighbors += _map[i, j];
+                }
+                else
+                    neighbors++;
+            }
+        }
+
+        return neighbors;
+    }
+
+    protected bool IsOutOfBounds(int x, int y)
+    {
+        return x < 0 || x >= _width || y < 0 || y >= _height;
+    }
 }
--- Assets/Scripts/Gameplay/RandomWalkGenerator.cs
+++ Assets/Scripts/Gameplay/RandomWalkGenerator.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class RandomWalkGenerator : MapGenerator
 {
+    private int _totalFloorCells;
+
     public RandomWalkGenerator()
         : base()
     {
+        _totalFloorCells = 50;
     }
 
-    public RandomWalkGenerator(int width, int height, int fillPercentage, bool autoSmoothing, int smoothing)
-        : base(width, height, fillPercentage, autoSmoothing, smoothing)
+    public RandomWalkGenerator(int width, int height, int fillPercentage, int smoothing, int totalFloorCells)
+        : base(width, height, fillPercentage, smoothing)
     {
+        _totalFloorCells = totalFloorCells;
     }
 
     public override int[,] CleanMapWalls(int wallThresholdSize)
@@ -21,11 +25,87 @@ public class RandomWalkGenerator : MapGenerator
 
     public override int[,] GenerateMap()
     {
+        int minX = 1;
+        int maxX = _width - 1;
+        int minY = 1;
+        int maxY = _height - 1;
+        int randomX = UnityEngine.Random.Range(minX, maxX);
+        int randomY = UnityEngine.Random.Range(minY, maxY);
+        MapGenerator.Cell currentCell = new Cell(randomX, randomY);
+
+        for (int i = 0; i < _width; i++)
+            for (int j = 0; j > _height; j++)
+                _map[i,j] = 1;
+
+        int floorCells = 0;
+        while (floorCells < _totalFloorCells)
+        {
+            Cell nextCell = GetNextCell(currentCell, UnityEngine.Random.Range(0, 3));
+
+            if (nextCell.x > 1 && nextCell.x < _width - 1 && nextCell.y > 0 && nextCell.y < _height - 1)
+            {
+                currentCell = nextCell;
+
+                if (_map[currentCell.x, currentCell.y] == 1)
+                {
+                    _map[currentCell.x, currentCell.y] = 0;
+                    floorCells++;
+                }
+            }
+        }
+
+        for (int i = 0; i < _smoothing; i++)
+            SmoothMap();
+
         return _map;
+
     }
 
     public override int[,] SmoothMap()
     {
+        int[,] newMap = _map;
+
+        for (int i = 0; i < _width; i++)
+        {
+            for (int j = 0; j < _height; j++)
+            {
+                int neighbors = GetNeighborsNumber(i, j, 1, 1);
+
+                if (neighbors > 4)
+                    newMap[i,j] = 1;
+                else if (neighbors < 4)
+                    newMap[i,j] = 0;
+            }
+        }
+
+        _map = newMap;
         return _map;
+    }
+
+    private Cell GetNextCell(Cell currentCell, int direction)
+    {
+        Cell nextCell = new Cell();
+
+        switch(direction)
+        {
+            case 0: //left
+                nextCell.x = currentCell.x - 1;
+                nextCell.y = currentCell.y;
+                break;
+            case 1: // up
+                nextCell.x = currentCell.x;
+                nextCell.y = currentCell.y - 1;
+                break;
+            case 2: // right
+                nextCell.x = currentCell.x + 1;
+                nextCell.y = currentCell.y;
+                break;
+            case 3: // down
+                nextCell.x = currentCell.x;
+                nextCell.y = currentCell.y + 1;
+                break;
+        }
+
+        return nextCell;
     }
 }
